63f09da8b231d57af5bb1fa902e7142f45272711
Patricia Souza <patricia.asouza@mercadolivre.com>
2021-09-16 09:48:28 -0300

    Initial commit

---
+++ src/exercicio1/model/Pessoa.java
@@ -0,0 +1,20 @@
+package exercicio1.model;
+
+public class Pessoa {
+
+    // classe pessoa com atributos solicitados
+
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private Integer idade;
+    private Double altura;
+
+
+
+
+
+
+
+
+}
